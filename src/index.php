37d9a392ecb2efd1a3de21b1114c844081f2f711
Daniel <DanielSebastianrf@gmail.com>
2023-11-06 23:35:48 -0500

    primer commit

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página de Presentación</title>
+</head>
+<body>
+    <h1>Esto es mi pagina funcionando con Docker Compose</h1>
+    <p>Se utilizo para crearlo php y mysql.</p>
+
+    <?php
+    $nombre = "Daniel Sebastian Reynoso Feliz";
+    $matricula = "2021-1571";
+    echo "<p>Mi nombre es $nombre.</p>";
+    echo "<p>Mi matricula es $matricula.</p>";
+    ?>
+
+</body>
+</html>
